87c86411a202ef929733b19144e7c1cd6c5fd017
Anvesh <anveshsahu15@gmail.com>
2026-03-09 08:59:22 +0530

    [Anvesh] : Updated application entry point to integrate controller layer for UC6

--- src/main/java/com/apps/addressbook/AddressBookAppApplication.java
+++ src/main/java/com/apps/addressbook/AddressBookAppApplication.java
@@ -1,13 +1,9 @@
 package com.apps.addressbook;
 
-import java.util.Scanner;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.apps.addressbook.model.AddressBook;
-import com.apps.addressbook.model.Contact;
+import com.apps.addressbook.controller.AddressBookController;
 
 @SpringBootApplication
 public class AddressBookAppApplication {
@@ -15,49 +11,7 @@ public class AddressBookAppApplication {
 
 		SpringApplication.run(AddressBookAppApplication.class, args);
 
-		AddressBook addressBook = new AddressBook();
-		Scanner scanner = new Scanner(System.in);
-
-		String choice;
-
-		do {
-
-			System.out.println("\nEnter Contact Details");
-
-			System.out.println("First Name:");
-			String firstName = scanner.nextLine();
-
-			System.out.println("Last Name:");
-			String lastName = scanner.nextLine();
-
-			System.out.println("Address:");
-			String address = scanner.nextLine();
-
-			System.out.println("City:");
-			String city = scanner.nextLine();
-
-			System.out.println("State:");
-			String state = scanner.nextLine();
-
-			System.out.println("Zip:");
-			String zip = scanner.nextLine();
-
-			System.out.println("Phone:");
-			String phone = scanner.nextLine();
-
-			System.out.println("Email:");
-			String email = scanner.nextLine();
-
-			Contact contact = new Contact(firstName, lastName, address, city, state, zip, phone, email);
-			addressBook.addContact(contact);
-			System.out.println("\nDo you want to add another contact? (yes/no)");
-			choice = scanner.nextLine();
-
-		} while (choice.equalsIgnoreCase("yes"));
-
-		System.out.println("\nAll Contacts:");
-		addressBook.displayContacts();
-
-		scanner.close();
+		AddressBookController controller = new AddressBookController();
+		controller.start();
 	}
 }
